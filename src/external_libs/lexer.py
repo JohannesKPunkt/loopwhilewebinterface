5b8e365dcd88f5a70d00987392d7e97326fcbfc5
Johannes Kern <johannes.kern@fau.de>
2019-09-12 00:13:07 +0200

    Removed demo application from lexer.py

--- src/external_libs/lexer.py
+++ src/external_libs/lexer.py
@@ -114,26 +114,3 @@ class Lexer(object):
             tok = self.token()
             if tok is None: break
             yield tok
-
-
-if __name__ == '__main__':
-    rules = [
-        ('\d+',             'NUMBER'),
-        ('[a-zA-Z_]\w+',    'IDENTIFIER'),
-        ('\+',              'PLUS'),
-        ('\-',              'MINUS'),
-        ('\*',              'MULTIPLY'),
-        ('\/',              'DIVIDE'),
-        ('\(',              'LP'),
-        ('\)',              'RP'),
-        ('=',               'EQUALS'),
-    ]
-
-    lx = Lexer(rules, skip_whitespace=True)
-    lx.input('erw = _abc + 12*(R4-623902)  ')
-
-    try:
-        for tok in lx.tokens():
-            print(tok)
-    except LexerError as err:
-        print('LexerError at position %s' % err.pos)
