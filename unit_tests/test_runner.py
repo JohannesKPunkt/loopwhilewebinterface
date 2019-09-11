8fdddcdb3c0453a4fdbb7af9c7b492539018e875
Johannes Kern <johannes.kern@fau.de>
2019-09-11 20:32:36 +0200

    added test_runner.py

---
+++ unit_tests/test_runner.py
@@ -0,0 +1,8 @@
+import unittest, sys
+
+sys.path.append("src")
+from SessionManagerTests import SessionManagerTests
+from IOToolsTests import IOToolsTests
+
+if __name__ == '__main__':
+    unittest.main()
