82e9af1a570fed27f24c469143dc05d666b0a1dd
Johannes Kern <johannes.kern@fau.de>
2019-11-13 23:56:04 +0100

    improvement: refactor source code view generation in DebuggerView

--- src/DebuggerView.py
+++ src/DebuggerView.py
@@ -8,32 +8,30 @@
 
 
 from external_libs.lexer import Lexer, LexerError
+from SourceCodeView import SourceCodeView
 
 from genshi.core import Markup
 import Logging
 
 logger = Logging.get_logger(__name__)
 
-# Lexer rules that are used to provide basic syntax highlighting
-_lexer_rules = [
-    ('def|enddef|LOOP|WHILE|do|enddo|if|then|else|in|out|aux', 'KEYWORD'),
-    ('loop|while',                                             'KEYWORD'),
-    ('#',                                                      'HASHTAG'),
-    ('\d+',                                                    'NUMBER'),
-    ('[a-zA-Z_](\w|\d)*',                                      'IDENTIFIER'),
-    ('\+|\-|\*|div|\%|==|<|<=|>|>=|!=',                        'OPERATOR'),
-    ('\(',                                                     'LP'),
-    ('\)',                                                     'RP'),
-    (';',                                                      'SEMICOLON'),
-    ('succ|pred',                                              'BUILTIN'),
-    (':=',                                                     'ASSIGNMENT'),
-    (',|:',                                                    'COLON'),
-    (' ',                                                      'SPACE'),
-    ('\t',                                                     'TAB'),
-    ('\n|\r\n',                                                'LINEBREAK'),
-    ('//',                                                     'ENTER_COMMENT'),
-    ('\S',                                                     'ILLEGAL_SYMBOL'),
-]
+class DebuggerSourceCodeView(SourceCodeView):
+    def begin_row_hook(self, linecount):
+        return "<tr><td class=\"debuggerCodeViewLineNo\">&nbsp;"\
+               + str(linecount) + "</td>"\
+               + "<td class=\"debuggerCodeViewSide\">"\
+               + "&nbsp;<img src=\"/img/transparentdot.png\" width=\"10px\" length=\"10px\" id=\"dot"\
+               + str(linecount) + "\" onclick=\"breakpoint_action(&quot;dot"\
+               + str(linecount) + "&quot;);\" />&nbsp;</td><td id=\"line" + str(linecount)+ "\">"
+
+    def end_row_hook(self, linecount):
+        return "</td></tr>"
+
+    def begin_view_hook(self):
+        return "<table class=\"debuggerCodeView\">"
+
+    def end_view_hook(self):
+        return "<tr style=\"height:100%;\"><td class=\"debuggerCodeViewLineNo\"></td><td class=\"debuggerCodeViewSide\"></td><td></td></tr></table>"
 
 
 class DebuggerView(dict):
@@ -49,7 +47,7 @@ class DebuggerView(dict):
             "debugger_varview.xml"]
         self["current_mode"] = "debugger"
         try:
-            self["debugger_view_content"] = Markup(DebuggerView._get_source_code_view(code))
+            self["debugger_view_content"] = Markup(DebuggerSourceCodeView().get_source_code_view(code))
         except LexerError as e:
             escaped = code[max(0,e.pos-10): min(len(code), e.pos+10)].replace("\n", " ")
             escaped = escaped.replace("\r", " ").replace("\t", " ")
@@ -58,59 +56,3 @@ class DebuggerView(dict):
             raise e
         self["session_id"] = session_id
         self["program_code"] = code
-
-    #  
-    #  name: getSourceCodeView
-    #  @param
-    #    source string with Loop/While source code to display
-    #    highlight_line line number that shall be highlighted (default=-1 means no line will be highlighted)
-    #  @return a valid html <table>-tag that can be inserted into the debugger.xhtml template
-    #  
-    @staticmethod
-    def _get_source_code_view(source, highlight_line=-1):
-        lexer = Lexer(_lexer_rules, False)
-        lexer.input(source)
-        
-        linecount = 1
-        in_comment = False#TODO: refactor redundant code
-        output = "<table class=\"debuggerCodeView\">"
-        output += "<tr><td class=\"debuggerCodeViewLineNo\">&nbsp;1</td>" \
-                  + "<td class=\"debuggerCodeViewSide\" onclick=\"breakpoint_action(&quot;dot1&quot;);\" >" \
-                  + "&nbsp;<img src=\"/img/transparentdot.png\" width=\"10px\" length=\"10px\" id=\"dot1\"/>&nbsp;"\
-                  + "</td><td>"
-        
-        for token in lexer.tokens():
-            if token.type == 'LINEBREAK':
-                linecount += 1
-                if highlight_line == linecount:
-                    bgcolor_str = " style=\"background-color: #787878;\""
-                else:
-                    bgcolor_str = ""
-                output += "</td></tr><tr><td class=\"debuggerCodeViewLineNo\">&nbsp;"\
-                          + str(linecount) + "</td>"\
-                          + "<td class=\"debuggerCodeViewSide\">"\
-                          + "&nbsp;<img src=\"/img/transparentdot.png\" width=\"10px\" length=\"10px\" id=\"dot"\
-                          + str(linecount) + "\" onclick=\"breakpoint_action(&quot;dot"\
-                          + str(linecount) + "&quot;);\" />&nbsp;</td><td id=\"line" + str(linecount)+ "\"" + bgcolor_str + ">"
-                in_comment = False
-            elif token.type == 'ENTER_COMMENT' or in_comment:
-                output += "<font color=\"#6f6564\" style=\"opacity:.8\">" + token.val + "</font>"#TODO font
-                in_comment = True
-            elif token.type == 'NUMBER':
-                output += "<font color=\"#d7432e\" style=\"opacity:.8\">" + token.val + "</font>"
-            elif token.type == 'OPERATOR':
-                output += "<font color=\"#d7910e\" style=\"opacity:.8\">" + token.val + "</font>"
-            elif token.type in ('IDENTIFIER', 'LP', 'RP', 'SEMICOLON', 'COLON', 'ASSIGNMENT', 'ILLEGAL_SYMBOL'):
-                output += "<font color=\"#F8FBEF\" style=\"opacity:.8\">" + token.val + "</font>"
-            elif token.type == 'SPACE':
-                output += "&nbsp;"
-            elif token.type == 'HASHTAG':
-                output += "#"
-            elif token.type == 'TAB':
-                output += "&nbsp;&nbsp;&nbsp;&nbsp;"
-            elif token.type == 'KEYWORD' or token.type == 'BUILTIN':
-                output += "<font color=\"#d7910e\" style=\"opacity:.8\">" + token.val + "</font>"
-            else:
-                raise RuntimeError("unknown token type: " + str(token))
-        output += "</td></tr></table>"
-        return output
