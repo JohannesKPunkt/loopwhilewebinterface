d08e46edc8ab0019a702f804b8125c32834f3365
Johannes Kern <Johannes.Kern@fau.de>
2020-01-28 11:41:12 +0100

    add lwre binary to repo
    This binary is built from the branch 'webinterface' from git8.cs.fau.de/software/loopwhile-yacc-interpreter

--- deliver.sh
+++ deliver.sh
@@ -6,8 +6,6 @@ INSTALL_DIR="/opt/loopwhile/"
 USER_SRC_DIR="/tmp/loopwhile_user_src/"
 HTTPD_DIR="/var/www/html/"
 
-#TODO deliver run.sh, lwre
-
 echo "copy python sources"
 cp -r src/ "$INSTALL_DIR/"
 chown $USER:$GROUP "$INSTALL_DIR/src/."
@@ -25,3 +23,8 @@ cp -r web/ $HTTPD_DIR
 echo "create user source dir if not existent"
 mkdir -p $USER_SRC_DIR
 chown $USER:$GROUP $USER_SRC_DIR
+
+echo "copy binary and scripts"
+cp lwre $INSTALL_DIR/
+cp run.sh $INSTALL_DIR/
+cp stop.sh $INSTALL_DIR/
