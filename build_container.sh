04f6fc88e5610df5846981531db960bd59c2b997
Johannes Kern <johannes.kern@fau.de>
2021-05-07 14:23:16 +0200

    Fix: Handling of logs in docker container

--- build_container.sh
+++ build_container.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # Creates a docker container that runs the lw service and nginx
 
-mkdir -p logs/loopwhile
-mkdir -p logs/nginx
-mkdir -p logs/supervisor
 docker build --tag loopwhile .
--- run_container.sh
+++ run_container.sh
@@ -3,9 +3,15 @@
 # The service will be available on port 80 of the host machine!
 # The log files are placed locally in logs/.
 
+# Import settings
+source settings.sh
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Local directory, where the log files are placed
-LOG_DIR=$SCRIPT_DIR/logs
+LOG_DIR="$SCRIPT_DIR/logs"
 
-sudo docker run --rm --name loopwhile -p 80:80 -v $LOG_DIR/supervisor:/var/log/supervisor -v $LOG_DIR/nginx:/var/log/nginx -v $LOG_DIR/loopwhile:/opt/loopwhile/logs loopwhile
+mkdir -p logs/loopwhile
+mkdir -p logs/nginx
+mkdir -p logs/supervisor
+sudo docker run --rm --name loopwhile -p 80:80 -v "$LOG_DIR/supervisor:/var/log/supervisor" -v "$LOG_DIR/nginx:/var/log/nginx" -v "$LOG_DIR/loopwhile:$LW_LOG_DIR" loopwhile
